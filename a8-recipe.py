d905f99cc9dfdd81c58555b5da4edc02a77fb9b6
glen@luminos <glen@luminos>
2017-06-08 15:23:05 -0400

    tweaks to get working from github

--- a8-recipe.py
+++ a8-recipe.py
@@ -3,6 +3,10 @@
 import os
 import subprocess
 import sys
+import xml.etree.ElementTree
+
+mavenSettingsFile = os.path.join(expanduser("~"), ".m2/settings.xml")
+mavenSettings = xml.etree.ElementTree.parse(mavenSettingsFile).getroot().find('servers').find('server')
 
 version="2.0.52-1611141017"
 
@@ -17,12 +21,15 @@ script_dir = parent_dir(script_file)
 packages_dir = parent_dir(script_dir)
 tools_dir = parent_dir(packages_dir)
 
+password = mavenSettings.find('password').text
+username = mavenSettings.find('username').text
+
 args = (
   [
     script_dir + "/coursier",
     "launch",
     "-r", 
-    "https://deployer:deployerbee@accur8.artifactoryonline.com/accur8/all/",
+    "https://" + username + ":" + password + "@accur8.artifactoryonline.com/accur8/all/",
     "-M", 
     "a8.recipe.RecipeMain",
     "a8:a8-recipe:" + version,
--- bootstrap.py
+++ bootstrap.py
@@ -5,14 +5,14 @@ from os.path import expanduser
 import subprocess
 
 
-def makedirs(dir):
-  if not os.path.exists(dir):
-    print "mkdir " + dir
-    os.makedirs(dir)
+def makedirs(d):
+  if not os.path.exists(d):
+    print "mkdir " + d
+    os.makedirs(d)
 
 
 def git_clone_a8_recipes():
-  run(["git", "clone", "https://gitlab.accur8.io/a8/recipe.git", a8_recipe_dir])
+  run(["git", "clone", "https://github.com/fizzy33/recipe", a8_recipe_dir])
 
 
 def run(args):
@@ -20,17 +20,16 @@ def run(args):
   subprocess.call(args)
 
 
-user_home = dir(expanduser("~"))
+def _dir(*path_parts):
+  return os.path.realpath(os.path.join(*path_parts))
 
 
-def dir(path_parts*):
-  return os.path.realpath(os.path.join(path_parts))
+user_home = _dir(expanduser("~"))
 
-
-tools_dir = dir(user_home, "tools-a8")
-packages_dir = dir(tools_dir, "packages")
-a8_recipe_dir = dir(packages_dir, "a8-recipe")
-bin_dir = dir(tools_dir, "bin")
+tools_dir = _dir(user_home, "tools-a8")
+packages_dir = _dir(tools_dir, "packages")
+a8_recipe_dir = _dir(packages_dir, "a8-recipe")
+bin_dir = _dir(tools_dir, "bin")
 
 
 makedirs(a8_recipe_dir)
