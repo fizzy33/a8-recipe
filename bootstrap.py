74ef172fa4f9b6a875507b3a26be341dfa8156f5
glen@luminos <glen@luminos>
2017-06-08 20:24:57 -0400

    tweaks to get working from github

--- bootstrap.py
+++ bootstrap.py
@@ -12,7 +12,7 @@ def makedirs(d):
 
 
 def git_clone_a8_recipes():
-  run(["git", "clone", "https://github.com/fizzy33/recipe", a8_recipe_dir])
+  run(["git", "clone", "https://github.com/fizzy33/a8-recipe", a8_recipe_dir])
 
 
 def run(args):
@@ -20,16 +20,16 @@ def run(args):
   subprocess.call(args)
 
 
-def _dir(*path_parts):
+def directory(*path_parts):
   return os.path.realpath(os.path.join(*path_parts))
 
 
-user_home = _dir(expanduser("~"))
+user_home = directory(expanduser("~"))
 
-tools_dir = _dir(user_home, "tools-a8")
-packages_dir = _dir(tools_dir, "packages")
-a8_recipe_dir = _dir(packages_dir, "a8-recipe")
-bin_dir = _dir(tools_dir, "bin")
+tools_dir = directory(user_home, "tools-a8")
+packages_dir = directory(tools_dir, "packages")
+a8_recipe_dir = directory(packages_dir, "a8-recipe")
+bin_dir = directory(tools_dir, "bin")
 
 
 makedirs(a8_recipe_dir)
@@ -41,5 +41,3 @@ git_clone_a8_recipes()
 
 # run the installer
 run([a8_recipe_dir + "/install.py"])
-
-
